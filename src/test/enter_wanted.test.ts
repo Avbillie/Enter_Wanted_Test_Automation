2e9d02c5119b50e296ab4357ddfd76484abd310d
Anthony_Billie_Jr <67110957+Avbillie@users.noreply.github.com>
2021-07-30 21:04:59 -0400

    final push

--- src/test/enter_wanted.test.ts
+++ src/test/enter_wanted.test.ts
@@ -6,22 +6,25 @@ import {
     until,
     WebDriver,
   } from "selenium-webdriver";
-import { beforeAll } from "@jest/globals";
+import { ENETDOWN } from "constants";
 const chromedriver = require("chromedriver");
 const driver = new Builder().withCapabilities(Capabilities.chrome()).build();
-let ew = new HomePage(driver)
-let scenario = {
+const ew = new HomePage(driver)
+const raceErrorMsg = 'The "Race" field should be 1 character long.';
+const headerErrorMsg = 'The "Header" field should be between 9 and 19 characters long.';
+const sexErrorMsg = 'The "Sex" field should be 1 character long.';
+const scenario = {
     one:`As a user I am limted to how many characters I 
     enter into each input field.`,
-    two:`As a user a prompt message should appear when I click the 
-    submit button with blank input fields`,
+    two:`As a user when I fill in header input field and leave the rest blank a error prompt message should 
+    appear for all other fields except the header field when the submit button is clicked`,
     three:`As a user all data within the input fields should be 
     cleared out when I click the clear button`,
     four:`As a user a prompt message should appear when 
     I click the submit button with blank input fields`,
     five:`As a user when I click the submit button with invalid data inside of the input fields I 
     should recieve error messages stating what is required.`,
-}
+};
 
 
 describe('Enter Wanted application', async () =>{
@@ -34,18 +37,31 @@ describe('Enter Wanted application', async () =>{
     })
     
     test(scenario.one, async ()=>{
-
+        await ew.insertHeaderInput('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+        await ew.clickSubmitBtn()   
+        let errorCount = await ew.viewAllErrorMsgs()
+        expect(errorCount.length).toBe(11)
     })
     test(scenario.two, async ()=>{
-
+        await ew.insertHeaderInput('Individualistically')
+        await ew.clickSubmitBtn()   
+        let errorCount = await ew.viewAllErrorMsgs()
+        expect(errorCount.length).toBe(10)
+     
     })
     test(scenario.three, async ()=>{
-
+        await ew.insertHeaderInput('Individualistically')
+        await ew.insertRaceInput('A')
+        await ew.insertSexInput('M')
+        await ew.clickCleartBtn()
+        expect(await ew.viewHeaderTxt()).toBe('');
+        expect(await ew.viewRaceTxt()).toBe('');
+        expect(await ew.viewtSexTxt()).toBe('');
     })
     test(scenario.four, async ()=>{
-        
-    })
-    test(scenario.five, async ()=>{
-
+        await ew.insertHeaderInput('Individualistically')
+        await ew.clickSubmitBtn()   
+        let errorCount = await ew.viewAllErrorMsgs()
+        expect(errorCount.length > 0).toBeTruthy()
     })
 })
--- src/test/enter_wanted.ts
+++ src/test/enter_wanted.ts
@@ -1,90 +1,88 @@
-import {
-  By,
-  until,
-  WebDriver,
-} from "selenium-webdriver";
+import { By, until, WebDriver } from "selenium-webdriver";
 
 export class HomePage {
   driver: WebDriver;
-  submitBtn: By = By.xpath("//button[contains(@id, saveBtn)]");
-  clearBtn: By = By.xpath("//button[contains(@id, clearBtn)]");
-  headerInput: By = By.xpath("//input[contains(@name, hdrHeader]");
-  sexInput: By = By.xpath("//input[contains(@name, sexInput]");
-  raceInput: By = By.xpath("//input[contains(@name, racInput]");
-  allInputFields: By = By.xpath("//input[contains(@class, inputField]");
-  homePage: string = "https://devmountain-qa.github.io/enter-wanted/1.4_Assignment/index.html";
-  errorMessages: By = By.xpath("//li[contains(@class, errorMessage)]");
-  headerErrorMsg: By = By.xpath("//li[contains(@class, errorMessage)][1]");
-  sexErrorMsg: By = By.xpath("//li[contains(@class, errorMessage)][5]");
-  raceErrorMsg: By = By.xpath("//li[contains(@class, errorMessage)][6]")
+  submitBtn: By = By.xpath("//button[contains(@id, 'saveBtn')]");
+  clearBtn: By = By.xpath("//button[contains(@id, 'clearBtn')]");
+  headerInput: By = By.xpath("//input[contains(@name, 'hdrInput')]");
+  sexInput: By = By.xpath("//input[contains(@name, 'sexInput')]");
+  raceInput: By = By.xpath("//input[contains(@name, 'racInput')]");
+  allInputFields: By = By.xpath("//input[contains(@class, 'inputField')]");
+  homePage: string =
+    "https://devmountain-qa.github.io/enter-wanted/1.4_Assignment/index.html";
+  errorMessages: By = By.xpath("//li[contains(@class, 'errorMessage')]");
+  headerErrorMsg: By = By.xpath("//li[contains(@class, 'errorMessage')][1]");
+  sexErrorMsg: By = By.xpath("//li[contains(@class, 'errorMessage')][5]");
+  raceErrorMsg: By = By.xpath("//li[contains(@class, 'errorMessage')][6]");
+  errorMsgs: any = [];
 
   constructor(driver?: WebDriver) {
     this.driver = driver;
-  };
+  }
 
   async navigate() {
-      this.driver.get(this.homePage);
-  };
+    this.driver.get(this.homePage);
+  }
 
   async quit() {
     this.driver.quit();
-  };
+  }
 
-  async clickSubmitBtn(){
+  async clickSubmitBtn() {
     await this.driver.wait(until.elementLocated(this.submitBtn));
-    await this.driver.findElement(this.submitBtn).click();
-  };
+    return await this.driver.findElement(this.submitBtn).click();
+  }
 
-  async clickCleartBtn(){
+  async clickCleartBtn() {
     await this.driver.wait(until.elementLocated(this.clearBtn));
-    await this.driver.findElement(this.clearBtn).click();
-  };
+    return await this.driver.findElement(this.clearBtn).click();
+  }
 
-  async insertAllInput(text: string) {
-    await this.driver.wait(until.elementsLocated(this.allInputFields));
-    let inputFields = await this.driver.findElements(this.allInputFields);
-    inputFields.forEach(async () => {
-      await this.driver.findElement(this.allInputFields).sendKeys(text);
-    });
-  };
-
-  async insertHeaderInput(text: string){
+  async insertHeaderInput(text: string) {
     await this.driver.wait(until.elementLocated(this.headerInput));
     await this.driver.findElement(this.headerInput).sendKeys(text);
-  };
+  }
+  async viewHeaderTxt() {
+    await this.driver.wait(until.elementLocated(this.headerInput));
+    return await this.driver.findElement(this.headerInput).getText();
+  }
 
-  async insertRaceInput(text: string){
+  async insertRaceInput(text: string) {
     await this.driver.wait(until.elementLocated(this.raceInput));
-    await this.driver.findElement(this.raceInput).sendKeys(text);
-  };
+    await this.driver.findElement(this.raceInput).click()
+    return await this.driver.findElement(this.raceInput).sendKeys(text);
+  }
+  async viewRaceTxt() {
+    await this.driver.wait(until.elementLocated(this.raceInput));
+    return await this.driver.findElement(this.raceInput).getText();
+  }
 
-  async insertSexInput(text: string){
+  async insertSexInput(text: string) {
     await this.driver.wait(until.elementLocated(this.sexInput));
-    await this.driver.findElement(this.sexInput).sendKeys(text);
-  };
+    await this.driver.findElement(this.sexInput).click()
+    return await this.driver.findElement(this.sexInput).sendKeys(text);
+  }
 
-  async viewAllErrorMsgs(){
+  async viewtSexTxt() {
+    await this.driver.wait(until.elementLocated(this.sexInput));
+    return await this.driver.findElement(this.sexInput).getText();
+  }
+
+  async viewAllErrorMsgs() {
     await this.driver.wait(until.elementsLocated(this.errorMessages));
-    let errors = await this.driver.findElements(this.errorMessages);
-    errors.forEach(async () =>{
-        await this.driver.wait(until.elementLocated(this.errorMessages)).getText()
-    })
+    return await this.driver.findElements(this.errorMessages);
   }
-
-  async viewHeaderErrorMsg(){
+  async viewHeaderErrorMsg() {
     await this.driver.wait(until.elementLocated(this.headerErrorMsg));
-    await this.driver.wait(until.elementLocated(this.headerErrorMsg)).getText()
+    return await this.driver.wait(until.elementLocated(this.headerErrorMsg)).getText();
   }
-  async viewSexErrorMsg(){
+  async viewSexErrorMsg() {
     await this.driver.wait(until.elementLocated(this.sexErrorMsg));
-    await this.driver.wait(until.elementLocated(this.sexErrorMsg)).getText()
+    return await this.driver.wait(until.elementLocated(this.sexErrorMsg)).getText();
   }
 
-  async viewRaceErrorMsg(){
+  async viewRaceErrorMsg() {
     await this.driver.wait(until.elementLocated(this.raceErrorMsg));
-    await this.driver.wait(until.elementLocated(this.raceErrorMsg)).getText()
+    return await this.driver.wait(until.elementLocated(this.raceErrorMsg)).getText();
   }
-
-
-
 }
